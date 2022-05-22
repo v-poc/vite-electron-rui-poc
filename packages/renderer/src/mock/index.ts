95915e111038aee2bca7be703cb95780dc34bfc0
NikoNi <bluepspower@qq.com>
2022-05-22 14:07:23 +0800

    refactor: code cleanup

--- packages/renderer/src/mock/index.ts
+++ packages/renderer/src/mock/index.ts
@@ -2,6 +2,5 @@ import { createProdMockServer } from "vite-plugin-mock/es/createProdMockServer";
 import todoMock from "./todo";
 
 export const setupMock = () => {
-  console.log("setupMock");
   createProdMockServer([...todoMock]);
 };
